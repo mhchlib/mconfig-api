c6965caa2aa0cffec56b6cdc1c9a160edf3f10f6
huchenhao <775656764@qq.com>
2020-11-02 02:07:50 +0800

    add code

--- api/sdk/config.pb.go
+++ api/sdk/config.pb.go
@@ -2,7 +2,7 @@
 // versions:
 // 	protoc-gen-go v1.24.0
 // 	protoc        v3.12.3
-// source: proto/config.proto
+// source: proto/v0/config.proto
 
 package sdk
 
@@ -30,13 +30,13 @@ type GetVRequest struct {
 	sizeCache     protoimpl.SizeCache
 	unknownFields protoimpl.UnknownFields
 
-	Configid string `protobuf:"bytes,1,opt,name=configid,proto3" json:"configid,omitempty"` //....
+	Configid string `protobuf:"bytes,1,opt,name=configid,proto3" json:"configid,omitempty"`
 }
 
 func (x *GetVRequest) Reset() {
 	*x = GetVRequest{}
 	if protoimpl.UnsafeEnabled {
-		mi := &file_proto_config_proto_msgTypes[0]
+		mi := &file_proto_v0_config_proto_msgTypes[0]
 		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
 		ms.StoreMessageInfo(mi)
 	}
@@ -49,7 +49,7 @@ func (x *GetVRequest) String() string {
 func (*GetVRequest) ProtoMessage() {}
 
 func (x *GetVRequest) ProtoReflect() protoreflect.Message {
-	mi := &file_proto_config_proto_msgTypes[0]
+	mi := &file_proto_v0_config_proto_msgTypes[0]
 	if protoimpl.UnsafeEnabled && x != nil {
 		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
 		if ms.LoadMessageInfo() == nil {
@@ -62,7 +62,7 @@ func (x *GetVRequest) ProtoReflect() protoreflect.Message {
 
 // Deprecated: Use GetVRequest.ProtoReflect.Descriptor instead.
 func (*GetVRequest) Descriptor() ([]byte, []int) {
-	return file_proto_config_proto_rawDescGZIP(), []int{0}
+	return file_proto_v0_config_proto_rawDescGZIP(), []int{0}
 }
 
 func (x *GetVRequest) GetConfigid() string {
@@ -83,7 +83,7 @@ type GetVResponse struct {
 func (x *GetVResponse) Reset() {
 	*x = GetVResponse{}
 	if protoimpl.UnsafeEnabled {
-		mi := &file_proto_config_proto_msgTypes[1]
+		mi := &file_proto_v0_config_proto_msgTypes[1]
 		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
 		ms.StoreMessageInfo(mi)
 	}
@@ -96,7 +96,7 @@ func (x *GetVResponse) String() string {
 func (*GetVResponse) ProtoMessage() {}
 
 func (x *GetVResponse) ProtoReflect() protoreflect.Message {
-	mi := &file_proto_config_proto_msgTypes[1]
+	mi := &file_proto_v0_config_proto_msgTypes[1]
 	if protoimpl.UnsafeEnabled && x != nil {
 		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
 		if ms.LoadMessageInfo() == nil {
@@ -109,7 +109,7 @@ func (x *GetVResponse) ProtoReflect() protoreflect.Message {
 
 // Deprecated: Use GetVResponse.ProtoReflect.Descriptor instead.
 func (*GetVResponse) Descriptor() ([]byte, []int) {
-	return file_proto_config_proto_rawDescGZIP(), []int{1}
+	return file_proto_v0_config_proto_rawDescGZIP(), []int{1}
 }
 
 func (x *GetVResponse) GetConfig() string {
@@ -119,42 +119,42 @@ func (x *GetVResponse) GetConfig() string {
 	return ""
 }
 
-var File_proto_config_proto protoreflect.FileDescriptor
+var File_proto_v0_config_proto protoreflect.FileDescriptor
 
-var file_proto_config_proto_rawDesc = []byte{
-	0x0a, 0x12, 0x70, 0x72, 0x6f, 0x74, 0x6f, 0x2f, 0x63, 0x6f, 0x6e, 0x66, 0x69, 0x67, 0x2e, 0x70,
-	0x72, 0x6f, 0x74, 0x6f, 0x12, 0x03, 0x77, 0x65, 0x62, 0x22, 0x29, 0x0a, 0x0b, 0x47, 0x65, 0x74,
-	0x56, 0x52, 0x65, 0x71, 0x75, 0x65, 0x73, 0x74, 0x12, 0x1a, 0x0a, 0x08, 0x63, 0x6f, 0x6e, 0x66,
-	0x69, 0x67, 0x69, 0x64, 0x18, 0x01, 0x20, 0x01, 0x28, 0x09, 0x52, 0x08, 0x63, 0x6f, 0x6e, 0x66,
-	0x69, 0x67, 0x69, 0x64, 0x22, 0x26, 0x0a, 0x0c, 0x47, 0x65, 0x74, 0x56, 0x52, 0x65, 0x73, 0x70,
-	0x6f, 0x6e, 0x73, 0x65, 0x12, 0x16, 0x0a, 0x06, 0x63, 0x6f, 0x6e, 0x66, 0x69, 0x67, 0x18, 0x01,
-	0x20, 0x01, 0x28, 0x09, 0x52, 0x06, 0x63, 0x6f, 0x6e, 0x66, 0x69, 0x67, 0x32, 0x40, 0x0a, 0x07,
-	0x4d, 0x43, 0x6f, 0x6e, 0x66, 0x69, 0x67, 0x12, 0x35, 0x0a, 0x0a, 0x47, 0x65, 0x74, 0x56, 0x53,
-	0x74, 0x72, 0x65, 0x61, 0x6d, 0x12, 0x10, 0x2e, 0x77, 0x65, 0x62, 0x2e, 0x47, 0x65, 0x74, 0x56,
-	0x52, 0x65, 0x71, 0x75, 0x65, 0x73, 0x74, 0x1a, 0x11, 0x2e, 0x77, 0x65, 0x62, 0x2e, 0x47, 0x65,
-	0x74, 0x56, 0x52, 0x65, 0x73, 0x70, 0x6f, 0x6e, 0x73, 0x65, 0x22, 0x00, 0x30, 0x01, 0x42, 0x09,
-	0x5a, 0x07, 0x61, 0x70, 0x69, 0x2f, 0x73, 0x64, 0x6b, 0x62, 0x06, 0x70, 0x72, 0x6f, 0x74, 0x6f,
-	0x33,
+var file_proto_v0_config_proto_rawDesc = []byte{
+	0x0a, 0x15, 0x70, 0x72, 0x6f, 0x74, 0x6f, 0x2f, 0x76, 0x30, 0x2f, 0x63, 0x6f, 0x6e, 0x66, 0x69,
+	0x67, 0x2e, 0x70, 0x72, 0x6f, 0x74, 0x6f, 0x12, 0x03, 0x77, 0x65, 0x62, 0x22, 0x29, 0x0a, 0x0b,
+	0x47, 0x65, 0x74, 0x56, 0x52, 0x65, 0x71, 0x75, 0x65, 0x73, 0x74, 0x12, 0x1a, 0x0a, 0x08, 0x63,
+	0x6f, 0x6e, 0x66, 0x69, 0x67, 0x69, 0x64, 0x18, 0x01, 0x20, 0x01, 0x28, 0x09, 0x52, 0x08, 0x63,
+	0x6f, 0x6e, 0x66, 0x69, 0x67, 0x69, 0x64, 0x22, 0x26, 0x0a, 0x0c, 0x47, 0x65, 0x74, 0x56, 0x52,
+	0x65, 0x73, 0x70, 0x6f, 0x6e, 0x73, 0x65, 0x12, 0x16, 0x0a, 0x06, 0x63, 0x6f, 0x6e, 0x66, 0x69,
+	0x67, 0x18, 0x01, 0x20, 0x01, 0x28, 0x09, 0x52, 0x06, 0x63, 0x6f, 0x6e, 0x66, 0x69, 0x67, 0x32,
+	0x40, 0x0a, 0x07, 0x4d, 0x43, 0x6f, 0x6e, 0x66, 0x69, 0x67, 0x12, 0x35, 0x0a, 0x0a, 0x47, 0x65,
+	0x74, 0x56, 0x53, 0x74, 0x72, 0x65, 0x61, 0x6d, 0x12, 0x10, 0x2e, 0x77, 0x65, 0x62, 0x2e, 0x47,
+	0x65, 0x74, 0x56, 0x52, 0x65, 0x71, 0x75, 0x65, 0x73, 0x74, 0x1a, 0x11, 0x2e, 0x77, 0x65, 0x62,
+	0x2e, 0x47, 0x65, 0x74, 0x56, 0x52, 0x65, 0x73, 0x70, 0x6f, 0x6e, 0x73, 0x65, 0x22, 0x00, 0x30,
+	0x01, 0x42, 0x09, 0x5a, 0x07, 0x61, 0x70, 0x69, 0x2f, 0x73, 0x64, 0x6b, 0x62, 0x06, 0x70, 0x72,
+	0x6f, 0x74, 0x6f, 0x33,
 }
 
 var (
-	file_proto_config_proto_rawDescOnce sync.Once
-	file_proto_config_proto_rawDescData = file_proto_config_proto_rawDesc
+	file_proto_v0_config_proto_rawDescOnce sync.Once
+	file_proto_v0_config_proto_rawDescData = file_proto_v0_config_proto_rawDesc
 )
 
-func file_proto_config_proto_rawDescGZIP() []byte {
-	file_proto_config_proto_rawDescOnce.Do(func() {
-		file_proto_config_proto_rawDescData = protoimpl.X.CompressGZIP(file_proto_config_proto_rawDescData)
+func file_proto_v0_config_proto_rawDescGZIP() []byte {
+	file_proto_v0_config_proto_rawDescOnce.Do(func() {
+		file_proto_v0_config_proto_rawDescData = protoimpl.X.CompressGZIP(file_proto_v0_config_proto_rawDescData)
 	})
-	return file_proto_config_proto_rawDescData
+	return file_proto_v0_config_proto_rawDescData
 }
 
-var file_proto_config_proto_msgTypes = make([]protoimpl.MessageInfo, 2)
-var file_proto_config_proto_goTypes = []interface{}{
+var file_proto_v0_config_proto_msgTypes = make([]protoimpl.MessageInfo, 2)
+var file_proto_v0_config_proto_goTypes = []interface{}{
 	(*GetVRequest)(nil),  // 0: web.GetVRequest
 	(*GetVResponse)(nil), // 1: web.GetVResponse
 }
-var file_proto_config_proto_depIdxs = []int32{
+var file_proto_v0_config_proto_depIdxs = []int32{
 	0, // 0: web.MConfig.GetVStream:input_type -> web.GetVRequest
 	1, // 1: web.MConfig.GetVStream:output_type -> web.GetVResponse
 	1, // [1:2] is the sub-list for method output_type
@@ -164,13 +164,13 @@ var file_proto_config_proto_depIdxs = []int32{
 	0, // [0:0] is the sub-list for field type_name
 }
 
-func init() { file_proto_config_proto_init() }
-func file_proto_config_proto_init() {
-	if File_proto_config_proto != nil {
+func init() { file_proto_v0_config_proto_init() }
+func file_proto_v0_config_proto_init() {
+	if File_proto_v0_config_proto != nil {
 		return
 	}
 	if !protoimpl.UnsafeEnabled {
-		file_proto_config_proto_msgTypes[0].Exporter = func(v interface{}, i int) interface{} {
+		file_proto_v0_config_proto_msgTypes[0].Exporter = func(v interface{}, i int) interface{} {
 			switch v := v.(*GetVRequest); i {
 			case 0:
 				return &v.state
@@ -182,7 +182,7 @@ func file_proto_config_proto_init() {
 				return nil
 			}
 		}
-		file_proto_config_proto_msgTypes[1].Exporter = func(v interface{}, i int) interface{} {
+		file_proto_v0_config_proto_msgTypes[1].Exporter = func(v interface{}, i int) interface{} {
 			switch v := v.(*GetVResponse); i {
 			case 0:
 				return &v.state
@@ -199,18 +199,18 @@ func file_proto_config_proto_init() {
 	out := protoimpl.TypeBuilder{
 		File: protoimpl.DescBuilder{
 			GoPackagePath: reflect.TypeOf(x{}).PkgPath(),
-			RawDescriptor: file_proto_config_proto_rawDesc,
+			RawDescriptor: file_proto_v0_config_proto_rawDesc,
 			NumEnums:      0,
 			NumMessages:   2,
 			NumExtensions: 0,
 			NumServices:   1,
 		},
-		GoTypes:           file_proto_config_proto_goTypes,
-		DependencyIndexes: file_proto_config_proto_depIdxs,
-		MessageInfos:      file_proto_config_proto_msgTypes,
+		GoTypes:           file_proto_v0_config_proto_goTypes,
+		DependencyIndexes: file_proto_v0_config_proto_depIdxs,
+		MessageInfos:      file_proto_v0_config_proto_msgTypes,
 	}.Build()
-	File_proto_config_proto = out.File
-	file_proto_config_proto_rawDesc = nil
-	file_proto_config_proto_goTypes = nil
-	file_proto_config_proto_depIdxs = nil
+	File_proto_v0_config_proto = out.File
+	file_proto_v0_config_proto_rawDesc = nil
+	file_proto_v0_config_proto_goTypes = nil
+	file_proto_v0_config_proto_depIdxs = nil
 }
